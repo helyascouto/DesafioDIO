13e637f4c5a9522d1341f4a3c3ecacb721e0d806
Elias <helyascouto@gmail.com>
2021-03-07 10:39:10 -0300

    add

---
+++ Programa para Validação de Notas/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ValidacaoNotas
+{
+    class Desafio
+    {
+
+        static void Main(string[] args)
+        {
+
+            double somaDasNotas = 0;
+            int contador = 0;
+            int escolha = 0;
+
+            do
+            {
+                double notaEntradaConsole = double.Parse(Console.ReadLine());
+                if (notaEntradaConsole < 0 || notaEntradaConsole > 10)
+                {
+                    Console.WriteLine("nota invalida");
+                    notaEntradaConsole = 0;
+                }
+                else
+                {
+                    contador++;
+                    somaDasNotas += notaEntradaConsole;
+                    notaEntradaConsole = 0;
+                }
+
+
+
+                if (contador == 2)
+                {
+                    double media = somaDasNotas / 2;
+                    Console.WriteLine("media = " + (media).ToString("N2"));
+                    Console.WriteLine("");
+                    Boolean op = false;
+                    do
+                    {
+                        escolha = 0;
+                        Console.WriteLine("Novo calculo (1-sim 2-nao)");
+                        escolha = Convert.ToInt32(Console.ReadLine());
+
+                        if (escolha == 1)
+                        {
+                            contador = 0;
+                            somaDasNotas = 0;
+                            notaEntradaConsole = 0;
+                            op = true;
+                        }
+                        else if (escolha == 2)
+                        {
+                            contador = 2;
+                            op = true;
+                        }
+                        else
+                        {
+                            contador = 0;
+                            somaDasNotas = 0;
+                            notaEntradaConsole = 0;
+                            op = false;
+                        }
+
+
+
+                    } while (op != true);
+                   
+                }
+
+
+            } while (contador < 2);
+
+        }
+    }
+}
---
+++ Substituição em Vetor I/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Substituição_em_Vetor_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] X = new int[10];
+            int y = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($" Digite um valor para X[{i}] ");
+                X[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (X[i] >= 0 && X[i] != null)
+                {
+                    Console.WriteLine($" X[{i}] = {X[i]}");
+                }
+                else
+                {
+                    X[i] = 1;
+                    Console.WriteLine($" X[{i}] = {X[i]}");
+                }
+                
+            }
+
+        }
+    }
+}
