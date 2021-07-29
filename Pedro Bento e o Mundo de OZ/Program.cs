479f602700d14c31ceb18bda3d30aa23eb511f65
Elias Abreu do Couto <73173398+helyascouto@users.noreply.github.com>
2021-07-29 08:11:41 -0300

    pedro bento

--- Pedro Bento e o Mundo de OZ/Program.cs
+++ Pedro Bento e o Mundo de OZ/Program.cs
@@ -10,14 +10,7 @@ namespace Pedro_Bento_e_o_Mundo_de_OZ
     {
         static void Main(string[] args)
         {
-            int qtdTeste = int.Parse(Console.ReadLine());
-            double kg;
-            for (int i = 0; i < qtdTeste; i++)
-            {
-                int qtdCasas = int.Parse(Console.ReadLine());
-                kg = Math.Pow(2, qtdCasas) / (12 * 1000);
-                Console.WriteLine("{0} kg", kg.ToString("0"));
-            }
+            
 
         }
     }
